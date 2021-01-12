bc3378aa49bcbbfdcb3c643959fa535c849ea04e
越川 佳祐 <keisuke.koshikawa@icare.jpn.com>
2021-01-12 21:46:45 +0900

    set application configs

--- config/application.rb
+++ config/application.rb
@@ -19,19 +19,22 @@ require "action_cable/engine"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module App
+module SampleApi
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-
-    # Only loads a smaller set of middleware suitable for API only apps.
-    # Middleware like session, flash, cookies can be added back manually.
-    # Skip views, helpers and assets when generating a new resource.
+    config.generators.system_tests = nil
+    config.time_zone = 'Tokyo'
+    config.i18n.default_locale = :ja
+    config.active_record.default_timezone = :local
     config.api_only = true
+
+    config.generators do |g|
+      g.skip_routes      true
+      g.test_framework   :rspec, fixture: false
+      g.view_specs       false
+      g.controller_specs false
+      g.helper           false
+      g.assets           false
+    end
   end
 end
