06f0e62ba944a92215532fa5acc76543617a53ff
越川 佳祐 <keisuke.koshikawa@icare.jpn.com>
2021-02-28 14:58:11 +0900

    add attached icatch

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,9 @@
 class Post < ApplicationRecord
   belongs_to :user
+
+  has_one_attached :icatch
+
+  def encoded_icatch
+    "data:image/png;base64,#{Base64.encode64(icatch.download)}" if icatch.attached?
+  end
 end
