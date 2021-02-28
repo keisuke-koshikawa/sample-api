7e152578896250ff9bdc962ccd5c21afdcb29240
越川 佳祐 <keisuke.koshikawa@icare.jpn.com>
2021-02-28 15:02:16 +0900

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+ActiveRecord::Base.transaction do
+  user = User.create!(
+    name: 'テストユーザー1',
+    nickname: 'テスト',
+    email: 'test-user+1@example.com',
+    password: 'password'
+  )
+
+  puts "successed create #{user.name}"
+
+  5.times do
+    post = Post.new(
+      user: user,
+      title: 'SPA作ってみた',
+      body: 'こんにちは！テストユーザー1です。今回はRails APIモードとVue.js 3 + TypeScriptを使って、SPAを作ってみましたので、紹介をさせてください。'
+    )
+    post.icatch.attach(io: File.open(Rails.root + 'db/seed_data/vue-rails.png'), filename: 'vue-rails.png')
+    post.save!
+  end
+end
